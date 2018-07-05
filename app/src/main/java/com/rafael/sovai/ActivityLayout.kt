41e3d56909bc0ab59e7543a72013c68ffde3d51e
RFMachado <rafael.machado@nodo.cc>
2018-07-05 09:48:25 -0300

    Added layout View

--- app/src/main/java/com/rafael/sovai/ActivityLayout.kt
+++ app/src/main/java/com/rafael/sovai/ActivityLayout.kt
@@ -1,9 +1,28 @@
 package com.rafael.sovai
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.util.AttributeSet
 import android.view.View
 
-class ActivityLayout: View {
+class ActivityLayout(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
+    val greenPaintStroke = Paint().apply {
+        color = Color.GREEN
+        style = Paint.Style.STROKE
+        strokeWidth = 10F
+    }
 
+    val rectangule = Rect().apply {
+        set(210, 125, 250,175)
+    }
 
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        canvas.drawRect(rectangule, greenPaintStroke)
+    }
 }
