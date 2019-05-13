5d59dd2e35cc4b0d7b359b10c1f3879c82c4cbff
Rafael Machado <rfmachado91@gmail.com>
2019-05-13 17:04:35 -0300

    Updated icon player 1 and player 2.

--- app/src/main/java/com/rafael/sovai/ActivityLayout.kt
+++ app/src/main/java/com/rafael/sovai/ActivityLayout.kt
@@ -3,52 +3,57 @@ package com.rafael.sovai
 import android.content.Context
 import android.graphics.*
 import android.graphics.Paint.ANTI_ALIAS_FLAG
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import com.rafael.sovai.models.Piece
 
 class ActivityLayout(context: Context, attrs: AttributeSet) : View(context, attrs) {
-    private val piece1 = Piece(context,0F, 0F)
-    private val piece2 = Piece(context,1F, 0F)
-    private val piece3 = Piece(context,0F, 1F)
-    private val piece4 = Piece(context,1F, 1F)
+    private val iconPlayerOne = BitmapFactory.decodeResource(context.resources, R.drawable.blue_android)!!
+    private val iconPlayerTwo = BitmapFactory.decodeResource(context.resources, R.drawable.red_android)!!
 
-    private val piece5 = Piece(context,6F, 6F)
-    private val piece6 = Piece(context,6F, 7F)
-    private val piece7 = Piece(context,7F, 6F)
-    private val piece8 = Piece(context,7F, 7F)
+    private val piece1 = Piece(0F, 0F, 1, iconPlayerOne)
+    private val piece2 = Piece(1F, 0F,1, iconPlayerOne)
+    private val piece3 = Piece(0F, 1F,1, iconPlayerOne)
+    private val piece4 = Piece(1F, 1F, 1, iconPlayerOne)
 
-    var matrix = arrayOf(
-            intArrayOf(1, 1, 0, 0, 0, 0, 0, 0),
-            intArrayOf(1, 1, 0, 0, 0, 0, 0, 0),
-            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
-            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
-            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
-            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
-            intArrayOf(0, 0, 0, 0, 0, 0, 1, 1),
-            intArrayOf(0, 0, 0, 0, 0, 0, 1, 1)
+    private val piece5 = Piece(6F, 6F, 2, iconPlayerTwo)
+    private val piece6 = Piece(6F, 7F, 2, iconPlayerTwo)
+    private val piece7 = Piece(7F, 6F, 2, iconPlayerTwo)
+    private val piece8 = Piece(7F, 7F, 2, iconPlayerTwo)
+
+    private var matrix = arrayOf(
+        intArrayOf(1, 1, 0, 0, 0, 0, 0, 0),
+        intArrayOf(1, 1, 0, 0, 0, 0, 0, 0),
+        intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+        intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+        intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+        intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+        intArrayOf(0, 0, 0, 0, 0, 0, 1, 1),
+        intArrayOf(0, 0, 0, 0, 0, 0, 1, 1)
     )
 
     private val listPiece: MutableList<Piece> = mutableListOf(piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8)
     var size: Int = 0
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        var multiply = 1
         val sizeWidth = width / 8
         val paint = createCheckerBoard(sizeWidth)
-        var mulitply = 1
-
-        if (size == 0) {
-            mulitply = sizeWidth
-        }
 
         canvas.drawPaint(paint)
 
+        if (size == 0)
+            multiply = sizeWidth
+
         listPiece.forEach { piece ->
-            canvas.drawBitmap(piece.bitmap, piece.getx() * mulitply, piece.gety() * mulitply, null)
-            piece.updateAxis(piece.getx() * mulitply, piece.gety() * mulitply)
+            canvas.drawBitmap(piece.icon, piece.getx() * multiply, piece.gety() * multiply, null)
+            piece.updateAxis(piece.getx() * multiply, piece.gety() * multiply)
         }
 
         size = sizeWidth
@@ -86,13 +91,13 @@ class ActivityLayout(context: Context, attrs: AttributeSet) : View(context, attr
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     piece.updateLastPosition(piece.movingX, piece.movingY)
-                    piece.isMoving = (event.x >= x && event.x <= x + piece.bitmap.width &&
-                            event.y >= y && event.y <= y + piece.bitmap.height)
+                    piece.isMoving = (event.x >= x && event.x <= x + piece.icon.width &&
+                            event.y >= y && event.y <= y + piece.icon.height)
 
                 }
                 MotionEvent.ACTION_MOVE -> {
                     if (piece.isMoving) {
-                        piece.updateAxis(event.x - piece.bitmap.width / 2, event.y - piece.bitmap.height / 2)
+                        piece.updateAxis(event.x - piece.icon.width / 2, event.y - piece.icon.height / 2)
                         invalidate()
                     }
                 }
--- app/src/main/java/com/rafael/sovai/models/Piece.kt
+++ app/src/main/java/com/rafael/sovai/models/Piece.kt
@@ -1,34 +1,28 @@
 package com.rafael.sovai.models
 
-import android.content.Context
-import android.graphics.BitmapFactory
-import com.rafael.sovai.R
+import android.graphics.Bitmap
 
-class Piece(context: Context, x: Float, y: Float) {
+class Piece(x: Float, y: Float, val player: Int, val icon: Bitmap) {
 
     var movingX = x
     var movingY = y
     var isMoving = false
-    var player: Int = 0
     var xmatrix: Int = 0
     var ymatrix: Int = 0
     var lastPositionX: Float = 0f
     var lastPositionY: Float = 0f
-
-    var bitmap = BitmapFactory.decodeResource(context.resources, R.mipmap.ic_bitmap)!!
-    var size: Int = 0
 
     fun updateAxis(x: Float, y: Float) {
         movingX = x
         movingY = y
     }
 
-    fun getx(): Float = movingX
-    fun gety(): Float = movingY
-
     fun updateLastPosition(x: Float, y: Float) {
         lastPositionX = x
         lastPositionY = y
     }
+
+    fun getx(): Float = movingX
+    fun gety(): Float = movingY
 
 }
